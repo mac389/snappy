eff3ea152d58a64e4cb30e0dbd1bbaa94b6d9574
Michael Chary <mac389@gmail.com>
2014-01-02 07:20:59 -0500

    Added skeleton of interface to python twitter wrapper

---
+++ Twitter.py
@@ -0,0 +1,9 @@
+import twitter, json
+
+READ = 'rb'
+tokens = json.load(open('tokens.json',READ))
+
+api = twitter.Api(**tokens['twitter'])
+
+#to find a social network, we must look by people, not topics
+#what about looking by region
